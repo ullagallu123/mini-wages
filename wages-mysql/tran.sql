9517e9c5e7845062ad5b1635ba825d75daeee0f8
sivaram <sivaram0434@gmail.com>
2024-08-06 12:13:53 +0530

    modifying the sql script

--- wages-mysql/tran.sql
+++ wages-mysql/tran.sql
@@ -1,4 +1,12 @@
 CREATE DATABASE IF NOT EXISTS transactions;
-CREATE USER 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
-GRANT ALL PRIVILEGES ON transactions.* TO 'expense'@'%';
+USE transactions;
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amount INT,
+    description VARCHAR(255)
+);
+
+CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
+GRANT ALL ON transactions.* TO 'expense'@'%';
 FLUSH PRIVILEGES;
